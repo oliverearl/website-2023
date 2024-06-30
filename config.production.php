a18b2f2ff4fa37db3a5129f7c0c96b4947bb3511
Oliver Earl <oliver@oliverearl.co.uk>
2024-06-30 19:10:33 +0100

    refactor: start-ups

--- config.production.php
+++ config.production.php
@@ -5,4 +5,10 @@ declare(strict_types=1);
 return [
     'baseUrl' => 'https://www.oliverearl.co.uk',
     'production' => true,
+    'switches' => [
+        'projects' => true,
+        'publications' => false,
+        'startups' => false,
+        'talks' => false,
+    ],
 ];
---
+++ source/start-ups.blade.php
@@ -0,0 +1,9 @@
+---
+title: Startups
+description: Information about my ongoing large side projects
+---
+@extends('_layouts.main')
+
+@section('body')
+  <h1>Startups</h1>
+@endsection
